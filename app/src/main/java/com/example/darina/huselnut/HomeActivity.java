46d7ee82311908fbb706a15f14db79a22005ac33
Ikis <darinafjordur@gmail.com>
2016-12-15 18:23:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/darina/huselnut/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.darina.huselnut;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}
