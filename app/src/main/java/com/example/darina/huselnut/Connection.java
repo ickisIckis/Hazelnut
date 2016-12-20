653b55e91fc4e1c5989e081f0535322444d461ff
Ikis <darinafjordur@gmail.com>
2016-12-20 08:18:59 +0300

    strings moved to variables

---
+++ app/src/main/java/com/example/darina/huselnut/Connection.java
@@ -0,0 +1,8 @@
+package com.example.darina.huselnut;
+
+/**
+ * Created by Darina on 19.12.16.
+ */
+
+public class Connection {
+}
--- app/src/main/java/com/example/darina/huselnut/HomeActivity.java
+++ app/src/main/java/com/example/darina/huselnut/HomeActivity.java
@@ -1,13 +1,16 @@
 package com.example.darina.huselnut;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.home_screen);
     }
 }
